453b050476b104bcef574b391c743260727a4fb5
salman5-star <salman80862378@gmail.com>
2026-03-04 23:11:20 +0500

    Add files via upload

---
+++ lab 1/lab task/LAB_01 TASK_08.cpp
@@ -0,0 +1,16 @@
+// Name: Salman Sohrab
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0730
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float aB = 10.2;  // Declaring float variable
+
+    cout << aB;  // Printing value of aB
+
+    return 0;  // Program ends successfully
+}
