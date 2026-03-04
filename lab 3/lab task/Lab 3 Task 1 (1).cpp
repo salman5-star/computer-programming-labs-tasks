12df9262f4fe447cfbb765dfac2cc31f8f246b10
salman5-star <salman80862378@gmail.com>
2026-03-04 23:14:35 +0500

    Add files via upload

---
+++ lab 3/lab task/Lab 3 Task 1 (1).cpp
@@ -0,0 +1,25 @@
+// Name: Salman Sohrab
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0730
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double voltage, current, resistance;
+
+    cout << "Enter voltage (V): ";
+    cin >> voltage;
+
+    cout << "Enter current (I): ";
+    cin >> current;
+
+    resistance = voltage / current;
+
+    cout << "Voltage: " << voltage << " V" << endl;
+    cout << "Current: " << current << " A" << endl;
+    cout << "Resistance: " << resistance << " Ohms" << endl;
+
+    return 0;
+}
