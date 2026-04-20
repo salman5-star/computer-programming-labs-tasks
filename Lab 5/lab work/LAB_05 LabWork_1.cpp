085e9d87af900518ebb272027b7c504d5dd66b83
salman5-star <salman80862378@gmail.com>
2026-04-20 20:23:11 +0500

    Add files via upload

---
+++ Lab 5/lab work/LAB_05 LabWork_1.cpp
@@ -0,0 +1,22 @@
+// Name:salman sohrab
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0730
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+
+    while(i <= 10)
+    {
+        cout << i << endl;
+        i++;
+    }
+
+    return 0;
+}
+
+
---
+++ Lab 5/lab work/LAB_05 LabWork_2.cpp
@@ -0,0 +1,23 @@
+// Name:salman sohrab
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0730
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+
+    do
+    {
+        cout << i << endl;
+        i++;
+    }
+    while(i <= 10);
+
+    return 0;
+}
+
+
